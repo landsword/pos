1da4b4e13ccc3f2375b5272ecd8e1745f2e58590
landsword <dreike1656@gmail.com>
2019-08-26 11:10:18 +0900

    190826 Update

--- pos.js
+++ pos.js
@@ -1,51 +1,70 @@
 var posdata = document.getElementById("data");
+var menu = document.getElementById("menu");
 var cal = document.getElementById("cal");
 
-var pushdata = function(data) {
-    for(let i = 0; i < data.length; i++) {
-        let button = document.createElement("input");
-        button.type = "button";
-        button.value = `${data[i].name} ${data[i].price}`;
-        button.className = "menu";
-        button.onclick = function() {
-            let div = document.createElement("div");
-            div.className = "menus";
-            div.innerHTML = this.value.slice(0, -4);
-            let textarea = document.createElement("textarea");
-            textarea.value = 1;
-            let upbutton = document.createElement("input");
-            upbutton.type = "button";
-            upbutton.value = "+";
-            upbutton.onclick = function() {
-                textarea.value++;
-                menu_price();
-            }
-            let downbutton = document.createElement("input");
-            downbutton.type = "button";
-            downbutton.value = "-";
-            downbutton.onclick = function() {
-                textarea.value <= 0 ? (textarea.value = 0) : textarea.value--;
-                menu_price();
-            }
-            let del_menu = document.createElement("input");
-            del_menu.type = "button";
-            del_menu.value = "X";
-            del_menu.onclick = function() {
-                div.remove();
-            }
-            let span = document.createElement("span");
-            let menu_price = function() {
-                span.innerHTML = data[i].price * textarea.value;
-            }
-            menu_price();
-            div.append(textarea);
-            div.append(upbutton);
-            div.append(downbutton);
-            div.append(del_menu);
-            div.append(span);
-            cal.append(div);
+for(let i = 0; i < data.length; i++) {
+    let button = document.createElement("input");
+    button.type = "button";
+    button.value = `${data[i].name} ${data[i].price}`;
+    button.onclick = function() {
+        let div = document.createElement("div");
+        let spantxt = document.createElement("span");
+        spantxt.innerHTML = data[i].name; //initialized name
+        
+        let span = document.createElement("span");
+
+        let textarea = document.createElement("textarea");
+        textarea.value = 1; // initialized value
+    
+        let upbutton = document.createElement("input");
+        upbutton.type = "button";
+        upbutton.value = "+";
+        upbutton.onclick = function() {
+            textarea.value++;
+            summenu();
         }
-        posdata.append(button);
+    
+        let downbutton = document.createElement("input");
+        downbutton.type = "button";
+        downbutton.value = "-";
+        downbutton.onclick = function() {
+            textarea.value < 1 ? 0 : textarea.value--;
+            summenu();
+        }
+    
+        let delmenu = document.createElement("input");
+        delmenu.type = "button";
+        delmenu.value = "x";
+        delmenu.onclick = function() {
+            div.remove();
+        }
+        let summenu = function() {
+            span.innerHTML = textarea.value * data[i].price;
+        }
+        summenu();
+        div.append(spantxt);
+        div.append(textarea);
+        div.append(upbutton);
+        div.append(downbutton);
+        div.append(delmenu);
+        div.append(span);
+
+        if (menu.innerText.indexOf(data[i].name) < 0) {
+            menu.prepend(div);
+        } else {
+            0;
+        }
     }
+    posdata.append(button);
 }
-pushdata(data);
+var sum = document.getElementById("sum");
+sum.onclick = function() {
+    let temp_span = document.getElementsByTagName("span");
+    let val = 0;
+    for(let i = 0; i < temp_span.length; i++) {
+        if (Number(temp_span[i].innerHTML) > 0) {
+            val = val + Number(temp_span[i].innerHTML);
+        }
+    }
+    cal.innerHTML = `Total: ${val}`;
+}
